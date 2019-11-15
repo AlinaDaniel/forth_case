e464b16da741f13da4f367c4f0b725acf69ffe72
AlinaDaniel <alina.daniel66@icloud.com>
2019-11-16 01:42:15 +0300

    The case is in a process. Work with TextBlob is done.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 #               Zaitseva A. (%),
 #               Daniel A.   (%).
 
+
+from textblob import TextBlob
+
+
 # Choosing the language.
 print('Choose language/ Выберите язык.\n1) Englishs/ Английский язык;\n2) '
       'Russian/ Русский язык.')
@@ -25,4 +29,21 @@ while True:
           'Russian/ Русский язык.')
     language = input('Input number/ Введите цифру: ')
 
-text = input("Введите текст:")
+text = input('Введите текст: ')
+
+text = TextBlob(text)
+polarity = text.polarity
+
+if 0.3 < polarity <= 1:
+    polarity = 'позитивный'
+elif -1 <= polarity < -0.3:
+    polarity = 'негативный'
+else:
+    polarity = 'нейтральный'
+
+subjectivity = text.subjectivity * 100
+objectivity = (100 - subjectivity)
+
+print('Тональность текста:', polarity)
+print('Cубъективность: ', '{:.1f}'.format(subjectivity), '%', sep='')
+print('Oбъективность: ', '{:.1f}'.format(objectivity), '%', sep='')
