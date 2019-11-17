dea6b71e1475ea3b8abe67932bb4c39aaedef535
zaitsvnast <zaitsvnast@gmail.com>
2019-11-17 11:38:49 +0700

    The case is in a process.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from textblob import TextBlob
 
 
 # Choosing the language.
-print('Choose language/ Выберите язык.\n1) Englishs/ Английский язык;\n2) '
+print('Choose language/ Выберите язык.\n1) English/ Английский язык;\n2) '
       'Russian/ Русский язык.')
 language = input('Input number/ Введите цифру: ')
 
@@ -30,6 +30,42 @@ while True:
     language = input('Input number/ Введите цифру: ')
 
 text = input('Введите текст: ')
+
+from textblob import TextBlob
+
+blob = TextBlob(text)
+count_sentens = len(blob.sentences)
+
+text = text.split()
+count_words = len(text)
+print(count_words)
+
+count_syllables = 0
+for letter in text:
+    if letter.lower() in 'аоиеёэыуюя':
+        count_syllables += 1
+
+ASL = count_sentens / count_words
+ASL = ("{:.3f}".format(ASL))
+ASW = count_words / count_syllables
+ASW = ("{:.3f}".format(ASW))
+FRE = 206.835 - (1.3 * ASL) - (60.1 * ASW)
+
+print('Предложений:', count_sentens)
+print('Слов:', count_words)
+print('Слогов:', count_syllables)
+print('Средняя длина предложения в словах:', ASL)
+print('Средняя длина слова в слогах:', ASW)
+print('Индекс удобочитаемости Флеша:', FRE)
+
+if FRE > 80:
+    print('Текст очень легко читается (для младших школьников).')
+elif 50 <= FRE <= 80:
+    print('Простой текст (для школьников).')
+elif 25 < FRE < 50:
+    print('Текст немного трудно читать (для студентов).')
+elif FRE <= 25:
+    print('Текст трудно читается (для выпускников ВУЗов).')
 
 text = TextBlob(text)
 polarity = text.polarity
